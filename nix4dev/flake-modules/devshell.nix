5ce9ecbb108f0258bef5556cf0f0fd43adddca7b
Jan Kouba <83823184+jan-kouba@users.noreply.github.com>
2024-08-01 15:21:08 +0200

    chore: Use nix4dev.envrc.watchDirectories to setup watches (#20)

--- nix4dev/flake-modules/devshell.nix
+++ nix4dev/flake-modules/devshell.nix
@@ -1,14 +1,8 @@
 {
   perSystem = {...}: {
-    nix4dev.envrc.preamble = ''
-      set_watch() {
-        TO_WATCH=""
-        mapfile -td "" TO_WATCH < <(find ./nix4dev-modules -type f -print0)
-        watch_file "''${TO_WATCH[@]}"
-        mapfile -td "" TO_WATCH < <(find ./tests -type f -print0)
-        watch_file "''${TO_WATCH[@]}"
-      }
-      set_watch
-    '';
+    nix4dev.envrc.watchDirectories = [
+      "nix4dev-modules"
+      "tests"
+    ];
   };
 }
