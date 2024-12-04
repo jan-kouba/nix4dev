605c4d4d9646f8f7c40728492645021fffa1f921
Jan Kouba <kouba.honza@gmail.com>
2024-12-04 23:42:46 +0100

    feat: Bump nixpkgs to 24.11

--- nix4dev-modules/project-flake/default.nix
+++ nix4dev-modules/project-flake/default.nix
@@ -24,7 +24,7 @@ in {
 
       config = {
         baseFlakeInputs = {
-          nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+          nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
           flake-parts = {
             url = "github:hercules-ci/flake-parts";
             inputs.nixpkgs-lib.follows = "nixpkgs";
