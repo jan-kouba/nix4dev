5fdbcf129ebe950b9410c927bf0055dd47282304
Jan Kouba <kouba.honza@gmail.com>
2025-01-26 22:59:18 +0100

    test(managed-files): Allow to specify multiple steps in managed files tests

--- flake-modules/managed-files/tests/test-cases.nix
+++ flake-modules/managed-files/tests/test-cases.nix
@@ -1,24 +1,28 @@
 {
   "can write file" = {
     expected = ./expected/write-file;
-    managedFilesConfig = {
-      files."test.nix".source.text = ''
-        {
-        foo = [  "barr"    ];
-        }
-      '';
-    };
+    managedFilesConfigs = [
+      {
+        files."test.nix".source.text = ''
+          {
+          foo = [  "barr"    ];
+          }
+        '';
+      }
+    ];
     enableTreefmt = false;
   };
 
   "can write formatted file" = {
     expected = ./expected/write-formatted-file;
-    managedFilesConfig = {
-      files."test.nix".source.text = ''
-        {
-        foo = [  "barr"    ];
-        }
-      '';
-    };
+    managedFilesConfigs = [
+      {
+        files."test.nix".source.text = ''
+          {
+          foo = [  "barr"    ];
+          }
+        '';
+      }
+    ];
   };
 }
