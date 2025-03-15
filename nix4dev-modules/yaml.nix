73ed88f8576e25be13147e93b32d4965cf9d0b6d
Jan Kouba <83823184+jan-kouba@users.noreply.github.com>
2025-03-15 22:55:11 +0100

    feat: Make formatted .yaml files look like YAML when they contain a valid JSON (#70)

--- nix4dev-modules/yaml.nix
+++ nix4dev-modules/yaml.nix
@@ -1,10 +1,35 @@
 {lib, ...}: {
-  perSystem = {config, ...}: {
+  perSystem = {
+    config,
+    pkgs,
+    ...
+  }: {
     options = {
       nix4dev.yaml.disable = lib.mkEnableOption "YAML support";
     };
 
     config = lib.mkIf (! config.nix4dev.yaml.disable) {
+      # Convert .yaml files formatted as JSON (which is still a valid YAML format)
+      # into more common YAML syntax (without curly braces, strings and field names wrapped in '"', ...).
+      # This step is handy, e.g. when you generate YAML files from nix expression
+      # using the `builtins.toJSON` function.
+      treefmt.settings.formatter = {
+        "yq-json" = {
+          command = "${pkgs.bash}/bin/bash";
+          options = [
+            "-euc"
+            ''
+              for file in "$@"; do
+                ${lib.getExe pkgs.yq-go} -i -P "$file"
+              done
+            ''
+            "--" # bash swallows the second argument when using -c
+          ];
+          includes = ["*.yaml" "*.yml"];
+          priority = -100;
+        };
+      };
+
       treefmt.programs.prettier = {
         enable = true;
 
