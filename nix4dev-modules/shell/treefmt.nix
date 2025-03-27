36d851cfd0c6d0d560e72fc68f99aa6e48d985a1
Jan Kouba <83823184+jan-kouba@users.noreply.github.com>
2025-03-27 14:37:14 +0100

    fix: Set the treefmt.projectRoot to point to the whole repository (#73)
    .. not just the nix4dev directory

--- nix4dev-modules/shell/treefmt.nix
+++ nix4dev-modules/shell/treefmt.nix
@@ -1,7 +1,10 @@
-{
+{self, ...}: {
   perSystem = {config, ...}: {
     treefmt = {
       projectRootFile = ".git/config";
+
+      # Set projectRoot to point to the whole repository, not just the `nix4dev` directory
+      projectRoot = self + "/..";
     };
 
     devshells.default = {
