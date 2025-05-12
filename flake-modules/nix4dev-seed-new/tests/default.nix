edd5b1dcb9cdbdad76bf9b5d489fb666d1bbc368
Jan Kouba <kouba.honza@gmail.com>
2025-05-12 23:36:19 +0200

    chore: WIP

--- flake-modules/nix4dev-seed-new/tests/default.nix
+++ flake-modules/nix4dev-seed-new/tests/default.nix
@@ -1,2 +1,8 @@
 {
+  perSystem = { pkgs, ...}: {
+    checks.nix4devSeedTests = pkgs.writeText "nix4dev-seed-tests" ''
+      Test outputs:
+      # ''${lib.strings.concatLines tests}
+    '';
+  };
 }
