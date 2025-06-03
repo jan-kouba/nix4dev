f506a799580b9d8f7f6effb549758291b810ce41
Jan Kouba <kouba.honza@gmail.com>
2025-06-03 17:17:41 +0200

    fix(test-lib): Delete empty parent directories of excluded files

--- flake-modules/test-lib/tests/excludes-files-from-test/default.nix
+++ flake-modules/test-lib/tests/excludes-files-from-test/default.nix
@@ -1,7 +1,12 @@
 {
   testDescription = "managed files can write file";
 
-  excludeFiles = [ "./test-exclude-file" ];
+  excludeFiles = [
+    "./test-exclude-file"
+    "na"
+    "sub1/sub2/foo"
+    "nasub1/nasub2/na"
+  ];
 
   steps = [
     {
@@ -9,6 +14,8 @@
         ''echo foo > $out/global-exclude-file''
         ''echo foo > $out/test-exclude-file''
         ''echo foo > $out/foo''
+        ''mkdir -p $out/sub1/sub2''
+        ''echo foo > $out/sub1/sub2/foo''
       ];
     }
   ];
