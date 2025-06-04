cbc837021396650885f44bbb70764e8afe0377b3
Jan Kouba <kouba.honza@gmail.com>
2025-06-04 11:22:35 +0200

    test: Replicate problem with deeply nested files

---
+++ flake-modules/test-lib/tests/works-with-deeply-nested-files/default.nix
@@ -0,0 +1,14 @@
+{
+  testDescription = "test lib works with deeply nested files";
+
+  steps = [
+    {
+      perSystem.test.commandsToExecute = [
+        ''mkdir -p   $out/sub1-2/sub2/''
+        ''mkdir -p   $out/sub1-2/sub2-2/''
+        ''echo foo > $out/sub1-2/sub2/foo''
+        ''echo foo > $out/sub1-2/sub2-2/foo''
+      ];
+    }
+  ];
+}
