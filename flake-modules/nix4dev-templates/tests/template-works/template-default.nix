aebec3ffda192d6a75e035666ea95e0db08dea39
Jan Kouba <83823184+jan-kouba@users.noreply.github.com>
2025-07-16 00:04:10 +0200

    feat: Import the nix4dev flake module when generating template (#106)
    Every template must configure the ./nix4dev/flake-modules/default.nix
    file using the extraFiles option. The file is then imported as flake
    module into the devshell used to generate template files.

--- flake-modules/nix4dev-templates/tests/template-works/template-default.nix
+++ flake-modules/nix4dev-templates/tests/template-works/template-default.nix
@@ -1,3 +1,8 @@
 {
+  perSystem.nix4dev.flake = {
+    extraInputs.dummy-flake.url = "github:jan-kouba/dummy-repo";
+    extraFlakeModules = [ "inputs.dummy-flake.flakeModules.default" ];
+  };
+
   # Add your config here
 }
