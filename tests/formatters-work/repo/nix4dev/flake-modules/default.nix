bc141eeb0ea92a8765f503dd315ebead4dc121b2
Jan Kouba <83823184+jan-kouba@users.noreply.github.com>
2025-07-26 19:07:57 +0200

    chore: Remove terraform overrides by opentofu in tests of formatters (#115)
    It is not needed anymore because the treefmt uses opentofu anyway

--- tests/formatters-work/repo/nix4dev/flake-modules/default.nix
+++ tests/formatters-work/repo/nix4dev/flake-modules/default.nix
@@ -1,21 +1,9 @@
 {
-  perSystem = { pkgs, ...}: {
+  perSystem = {
     treefmt.settings.global.excludes = [
       "expected/**"
     ];
 
     nix4dev.terraform.enable = true;
-
-    nix4dev.overlays = [
-      (prev: self: {
-        terraform = pkgs.writeShellApplication {
-          name = "terraform";
-          runtimeInputs = [pkgs.opentofu];
-          text = ''
-            exec tofu "$@"
-          '';
-        };
-      })
-    ];
   };
 }
